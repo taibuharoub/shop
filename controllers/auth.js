7e8b8d33151e3ad16710fbe10936f8adef256808
taibuharoub <haroubtaibu@gmail.com>
2021-04-14 00:32:43 +0300

    Sending Email

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,14 @@
 const bcrypt = require("bcryptjs");
+const nodemailer = require("nodemailer");
+const sendgridTransport = require("nodemailer-sendgrid-transport");
 
 const User = require("../models/user");
+
+const transporter = nodemailer.createTransport(sendgridTransport({
+  auth: {
+    api_key: ""
+  }
+}))
 
 exports.getLogin = (req, res, next) => {
   let message = req.flash("error")
@@ -84,7 +92,16 @@ exports.postSignup = (req, res, next) => {
         })
         .then((result) => {
           res.redirect("/login");
-        });
+          return transporter.sendMail({
+            to: email,
+            from: "",
+            subject: "Signup Successded!",
+            html: `<h1>You successfully signed up!</h1>`
+          })
+        })
+        .catch(err => {
+          console.log(err);
+        })
     })
     .catch((err) => {
       console.log(err);
