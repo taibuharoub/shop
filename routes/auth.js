4d76cc136d442e411770510538668029649798ab
taibuharoub <haroubtaibu@gmail.com>
2021-04-16 22:48:33 +0300

    Adding Validation

--- routes/auth.js
+++ routes/auth.js
@@ -1,20 +1,29 @@
-const express = require('express');
+const express = require("express");
 // const expValidator = require("express-validator/check"); //import a sub package
-const {check} = require("express-validator/check"); //import a sub package
+const { check } = require("express-validator"); //import a sub package
 
-const authController = require('../controllers/auth');
+const authController = require("../controllers/auth");
 
 const router = express.Router();
 
-router.get('/login', authController.getLogin);
+router.get("/login", authController.getLogin);
 
-router.get('/signup', authController.getSignup);
+router.get("/signup", authController.getSignup);
 
-router.post('/login', authController.postLogin);
+router.post("/login", authController.postLogin);
 
-router.post('/signup', check("email").isEmail().withMessage("Please enter a valid email."), authController.postSignup);
+router.post(
+  "/signup",
+  check("email").isEmail().withMessage("Please enter a valid email.").custom((value, {req}) => {
+      if (value === "pp@pp.com") {
+          throw new Error("This email address is forbidden");
+      }
+      return true;
+  }),
+  authController.postSignup
+);
 
-router.post('/logout', authController.postLogout);
+router.post("/logout", authController.postLogout);
 
 router.get("/reset", authController.getReset);
 router.post("/reset", authController.postReset);
