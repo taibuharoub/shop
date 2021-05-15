b7000aa7a010f5da4f1aed4a7899cacce9fb5287
Taibu Haroub <haroubtaibu@gmail.com>
2021-05-15 11:42:26 +0300

    Configure multer to adjust filenames

--- app.js
+++ app.js
@@ -26,12 +26,21 @@ const store = new MongoDBStore({
 
 const csrfProtection = csrf();
 
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString() + '-' + file.originalname);
+  }
+})
+
 app.set("view engine", "ejs");
 app.set("views", "views");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(express.urlencoded({extended: true}));
-app.use(multer({dest: "images"}).single("image"));
+app.use(multer({storage: fileStorage}).single("image"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(session({
   secret: "Lazy Dog",
