62097c571fe4e7cd7abafa4cadd0d9a8634e7eef
Taibu Haroub <haroubtaibu@gmail.com>
2021-05-18 12:24:57 +0300

    Add pagination to other pages

--- controllers/shop.js
+++ controllers/shop.js
@@ -11,13 +11,27 @@ const { Error } = require("mongoose");
 const ITEMS_PER_PAGE = 1;
 
 exports.getProducts = (req, res, next) => {
+  const page = +req.query.page || 1;
+  let totalItems;
+
   Product.find()
-    .then((products) => {
-      console.log(products);
+    .countDocuments()
+    .then((numProducts) => {
+      totalItems = numProducts;
+      return Product.find()
+      .skip((page - 1) * ITEMS_PER_PAGE)
+      .limit(ITEMS_PER_PAGE)
+    }).then((products) => {
       res.render("shop/product-list", {
         prods: products,
         pageTitle: "All Products",
         path: "/products",
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPerviousPage: page > 1,
+        nextPage: page + 1,
+        perviousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
       });
     })
     .catch((err) => {
