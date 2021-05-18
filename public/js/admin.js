eedfcd26f8d3e01bf10ce9844f7c7638e1dfb9df
Taibu Haroub <haroubtaibu@gmail.com>
2021-05-18 13:28:07 +0300

    change the product delete functionality to an async request

---
+++ public/js/admin.js
@@ -0,0 +1,7 @@
+const deleteProduct = (btn) => {
+    // console.log(btn.parentNode);
+    // console.log(btn.parentNode.querySelector("[name=productId]"));
+    // console.log(btn.parentNode.querySelector("[name=productId]").value);
+    const prodId = btn.parentNode.querySelector("[name=productId]").value;
+    const csrf = btn.parentNode.querySelector("[name=_csrf]").value;
+}
